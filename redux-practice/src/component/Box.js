4578c4608b763c0ef06a06ddb1238a526d9f1d24
choijongan <mmm7134@naver.com>
2022-10-22 00:39:12 +0900

    리덕스 예제2 (GrandSonBox생성)

--- redux-practice/src/component/Box.js
+++ redux-practice/src/component/Box.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import GrandSonBox from './GrandSonBox'
 
 const Box = () => {
     let count = useSelector((state)=> state.count)
   return (
-    <div>this is box {count}</div>
+    <div>this is box {count}
+        <GrandSonBox />
+    </div>
   )
 }
 
